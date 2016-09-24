56ec8ddb727f134c55a6cd8ecb2b8abee217f17e
eqguo <ellis.q.guo@gmail.com>
2016-09-24 17:46:43 -0400

    save to disk instead of memory

--- Photographer.cs
+++ Photographer.cs
@@ -1,47 +1,52 @@
 using UnityEngine;
 
 public class Photographer : MonoBehaviour {
+  string FILENAME = "translationImage.jpg";
   PhotoCapture photo = null;
+
+  // Create capture object
   void Start() {
     PhotoCapture.CreateAsync(false, OnPhotoCaptureCreated);
   }
 
+  // Set up camera parameters, enter photo mode
   void OnPhotoCaptureCreated(PhotoCapture capture) {
     photo = capture;
 
     Resolution cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
 
-    CameraParameters c = new CameraParameters();
-    c.hologramOpacity = 0.0f;
-    c.cameraResolutionWidth = cameraResolution.width;
-    c.cameraResolutionHeight = cameraResolution.height;
-    c.pixelFormat = CapturePixelFormat.BGRA32;
+    CameraParameters cameraParams = new CameraParameters();
+    cameraParams.hologramOpacity = 0.0f;
+    cameraParams.cameraResolutionWidth = cameraResolution.width;
+    cameraParams.cameraResolutionHeight = cameraResolution.height;
+    cameraParams.pixelFormat = CapturePixelFormat.BGRA32;
 
     captureObject.StartPhotoModeAsync(c, false, OnPhotoModeStarted);
   }
 
+  // Cleanup
   void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result) {
        photoCaptureObject.Dispose();
        photoCaptureObject = null;
    }
 
+  // Take pic, save file 
   void OnPhotoModeStarted(PhotoCapture.PhotoCaptureResult result) {
     if (result.success) {
-      photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
+      string filepath = System.IO.Path.Combine(Application.persistentDataPath, FILENAME);
+      photoCaptureObject.TakePhotoAsync(filepath, PhotoCaptureFileOutputFormat.JPG, OnCapturedPhotoToMemory);
     } else {
-      Debug.LogError("Unable to start photo mode!");
+      Debug.LogError("Error starting photo mode.");
     }
   }
 
-  void OnCapturedPhotoToMemory(PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame) {
+  // Cleanup on completion
+  void OnCapturedPhotoToDisk(PhotoCapture.PhotoCaptureResult result) {
     if (result.success) {
-      Resolution cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
-      Texture2D targetTexture = new Texture2D(cameraResolution.width, cameraResolution.height);
-      photoCaptureFrame.UploadImageDataToTexture(targetTexture);
 
-      // add to shape
+      // http GET
 
+      photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
     }
-    photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
   }
 }
