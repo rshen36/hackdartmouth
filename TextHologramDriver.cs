bb9d539b5c0380d60610ba4089f73a0a29911067
Byrne Hollander <bhollander823@gmail.com>
2016-09-24 18:17:03 -0400

    fixing driver

--- TextHologramDriver.cs
+++ TextHologramDriver.cs
@@ -6,9 +6,9 @@ public class TextHologramDriver : MonoBehaviour {
     static void Main(string[] args) {
         while (true) {
             if (Input.GetKeyDown("c")) { CreateMesh("cee", 100); }
-            if (Input.GetKeyDown("f") { CreateMesh("eff", 300); }
-            if Input.GetKeyDown("l") { CreateMesh("el", 600); }
-            if Input.GetKey"escape") { Application.Quit(); }
+            if (Input.GetKeyDown("f")) { CreateMesh("eff", 300); }
+            if (Input.GetKeyDown("l")) { CreateMesh("el", 600); }
+            if (Input.GetKey"escape")) { Application.Quit(); }
         }
     }
 }
